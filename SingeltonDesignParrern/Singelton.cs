d9e26c96871862bdc84b57782e1bb5f6aa71f4c1
abdoo431 <34003275+abdoo431@users.noreply.github.com>
2022-07-14 02:35:31 +0200

    lazy loading
    convert eager loading to lazy loading by lazy keyword

--- SingeltonDesignParrern/Singelton.cs
+++ SingeltonDesignParrern/Singelton.cs
@@ -9,13 +9,13 @@ namespace SingeltonDesignParrern
     public sealed class Singelton
     {
         private static int counter = 0;
-        private static readonly Singelton instance = new Singelton();
+        private static readonly Lazy<Singelton> instance = new Lazy<Singelton>(()=>new Singelton());
         private static readonly object o = new object();
         public static Singelton GetInstans
         {
             get
             {
-                return instance;
+                return instance.Value;
             }
         }
         private Singelton()
